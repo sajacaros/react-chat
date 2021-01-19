f8782037bffa2e119df2ca4107b7f3afc372f811
dukim <dukim@bnpinnovation.com>
2021-01-19 18:43:38 +0900

    search 작업중

--- src/components/ChatPage/MainPanel/MainPanel.js
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MessageForm from './MessageForm'
 import MessageHeader from './MessageHeader'
 import MessageBody from './MessageBody'
 
 
 function MainPanel() {
+  const [searchTerm, setSearchTerm] =  useState("");
+  const [searchResults, setSearchResults] =  useState([]);
+  const [searchLoading, setSearchLoading] =  useState(false);
+
   return (
     <div style={{
       padding: '1rem 2rem 0 2rem'
