5642d57ca63e2281ffbb40a656ef20bcb94ef25a
dukim <dukim@bnpinnovation.com>
2021-01-19 11:40:11 +0900

    message display

--- src/components/ChatPage/MainPanel/Message.js
+++ src/components/ChatPage/MainPanel/Message.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Media from 'react-bootstrap/Media';
+import moment from 'moment';
 
-function Message(props) {
+function Message({message, user}) {
+  const timeFromNow = timestamp=>moment(timestamp).fromNow();
+  const isImage = message => {
+    return message.hasOwnProperty("image");
+  }
   return (
-    <div>
-      {props.message.content}
-    </div>
+    <Media>
+      <img 
+        style={{borderRadius: '10px', margin: '5px 1px 1px 1px'}}
+        width={40}
+        height={40}
+        className="mr-3"
+        src={message.user.image}
+        alt={message.user.name}
+      />
+      <Media.Body>
+        <h6>
+          {message.user.name}
+          <span style={{fontSize: '10px', color:'gray', margin: '0px 0px 5px 5px'}}>{timeFromNow(message.timestamp)}</span>
+        </h6>
+        {isImage(message)?
+          <img style={{masWidth:'200px'}} alt="이미지" src={message.image} />
+          :
+          <p>{message.content}</p>
+        }
+      </Media.Body>
+    </Media>
   );
 }
 
